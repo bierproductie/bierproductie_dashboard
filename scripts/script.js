fbab0cba5c3d9ffc43a7e8621fcf2b8b3fb7265d
kepet19 <kevinmeejach@gmail.com>
2020-11-13 12:47:50 +0100

    Made it so button can be pressed now

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,33 @@
 let time = document.getElementsByClassName("ptime");
 time[0].addEventListener("click", () => {
-  console.log("Ale production");
+    console.log("Ale production");
 });
 
-setInterval(function()
-  { time[0].innerHTML = new Date().toString().slice(16, 25);}, 1000);
 
+function buttonFac(name){
+    let temp = document.getElementsByClassName(name);
+    temp[0].addEventListener("click", () => {
+        RemoveMarked();
+        temp[0].className += ' marked';
+    });
+}
+
+buttonFac('temp');
+buttonFac('state');
+buttonFac('humidity');
+buttonFac('productsPerMin');
+buttonFac('amountToProduce');
+buttonFac('acceptableProducts');
+buttonFac('vibration');
+buttonFac('DefectProducts');
+buttonFac('produced');
+
+function RemoveMarked(){
+    let marked = document.getElementsByClassName('marked')[1];
+    console.log(marked.className);
+    marked.className = marked.className.split(' ')[0];
+}
+
+setInterval(function() { 
+    time[0].innerHTML = new Date().toString().slice(16, 25);
+}, 0000);
