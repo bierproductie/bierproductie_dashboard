772c5971989602a73ff6634ba21650882551280d
kepet19 <kevinmeejach@gmail.com>
2020-11-15 23:42:20 +0100

    add mainpage, sortedfiles, need to fix productionTime

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
-let time = document.getElementsByClassName("ptime");
-time[0].addEventListener("click", () => {
+
+let time = document.getElementsByClassName("ptime")[0];
+time.addEventListener("click", () => {
     console.log("Ale production");
 });
 
 
 function buttonFac(name){
-    let temp = document.getElementsByClassName(name);
-    temp[0].addEventListener("click", () => {
+    let temp = document.getElementsByClassName(name)[0];
+    temp.addEventListener("click", () => {
         RemoveMarked();
-        temp[0].className += ' marked';
+        temp.classList.add("marked");
     });
 }
 
@@ -23,11 +24,34 @@ buttonFac('DefectProducts');
 buttonFac('produced');
 
 function RemoveMarked(){
-    let marked = document.getElementsByClassName('marked')[1];
+    let marked = document.getElementsByClassName('marked')[0];
     console.log(marked.className);
-    marked.className = marked.className.split(' ')[0];
+    marked.classList.remove("marked");
 }
 
 setInterval(function() { 
-    time[0].innerHTML = new Date().toString().slice(16, 25);
-}, 0000);
+    time.children[1].innerHTML = new Date().toString().slice(16, 25);
+}, 1);
+
+select_items = document.getElementsByClassName("select-items")[0];
+document.getElementById("batchNr").addEventListener("click", () => {
+    select_items.classList.toggle("select-hide");
+});
+
+let data = [ 2312, 2390, 2039, 1023, 2093, 1049, 1923, 231];
+data.forEach( (batch) => {
+    let div = document.createElement("div");
+    div.innerText = batch;
+    div.addEventListener("click", () =>{
+        document.getElementsByClassName("batchNr")[0].value = batch;
+        console.log("shit");
+    });
+    select_items.appendChild(div);
+});
+
+document.addEventListener("click", (element) => {
+    if(element.target == document.getElementById("batchNr")){
+    }else {
+    select_items.classList.add("select-hide");
+    }
+});
