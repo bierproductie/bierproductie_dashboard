d0252c577cbc9e5f24a21b1e0d508bce4366a250
kepet19 <kevinmeejach@gmail.com>
2020-11-18 12:59:28 +0100

    Add Listingevent to BatchNr, changed so the border is the same size

--- scripts/script.js
+++ scripts/script.js
@@ -38,20 +38,24 @@ document.getElementById("batchNr").addEventListener("click", () => {
     select_items.classList.toggle("select-hide");
 });
 
+document.getElementsByClassName("batchNr")[0].addEventListener("click", () => {
+    select_items.classList.toggle("select-hide");
+});
+
 let data = [ 2312, 2390, 2039, 1023, 2093, 1049, 1923, 231];
 data.forEach( (batch) => {
     let div = document.createElement("div");
     div.innerText = batch;
     div.addEventListener("click", () =>{
         document.getElementsByClassName("batchNr")[0].value = batch;
-        console.log("shit");
     });
     select_items.appendChild(div);
 });
 
 document.addEventListener("click", (element) => {
-    if(element.target == document.getElementById("batchNr")){
+    if(element.target == document.getElementById("batchNr") ||
+        element.target == document.getElementsByClassName("batchNr")[0]){
     }else {
-    select_items.classList.add("select-hide");
+        select_items.classList.add("select-hide");
     }
 });
