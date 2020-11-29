088cef9bb6dd2a83e61bdca6746d09b246f2d070
kepet19 <kevinmeejach@gmail.com>
2020-11-29 01:16:41 +0100

    Add inintal javascript to settings.html

---
+++ scripts/ui-elements/recipe.js
@@ -0,0 +1,57 @@
+export { 
+    recipe,
+}
+
+console.log("Loading recipe module!");
+
+class recipe {
+    constructor(name, maxSpeed, recommenedSpeed){
+        this.dSpeed = document.getElementById("speed");
+        this.maxSpeed = maxSpeed;
+        this.recommenedSpeed = recommenedSpeed;
+
+        const list = document.getElementById("recipe-list");
+
+        this.li = document.createElement("li");
+        this.li.classList.add("recipe-el");
+        this.li.addEventListener("click", () => {
+            this.updateView();
+        });
+
+
+        // Create top with the name
+        const pTop = document.createElement("p");
+        pTop.innerText = name.toUpperCase();
+        pTop.classList.add("recipe-top");
+
+        this.li.appendChild(pTop);
+
+
+        // Create bottom div
+        const div = document.createElement("div");
+        div.classList.add("recipe-bottom");
+        const pLeft = document.createElement("p");
+        pLeft.innerText = "MAX SPEED: "+maxSpeed;
+        pLeft.classList.add("recipe-left");
+        div.appendChild(pLeft);
+        let pRight = document.createElement("p");
+        pRight.innerText = "RECOMMEND SPEED: "+recommenedSpeed;
+        pRight.classList.add("recipe-right");
+        div.appendChild(pRight);
+
+        this.li.appendChild(div);
+
+        list.appendChild(this.li);
+    }
+
+    updateView(){
+        this.dSpeed.placeholder = "Recommend: "+this.recommenedSpeed;
+        this.dSpeed.max = this.maxSpeed;
+        //Markeding with html
+        let remove = document.getElementsByClassName('marked');
+        if (remove.length > 0){
+            remove[0].classList.remove("marked");
+        }
+        this.li.classList.add("marked");
+    }
+}
--- scripts/util.js
+++ scripts/util.js
@@ -5,20 +5,12 @@ export {
 };
 
 function isNumber(number) {
-    if (isNaN(number) || typeof number != "number") {
-        return false;
-    } else {
-        return true;
-    }
+    return !(isNaN(number) || typeof number != "number");
 }
 
 function isBetween(number, bottom, top) {
-    if (number > top || number < bottom || !isNumber(number) || 
-        !isNumber(bottom) || !isNumber(top) ) {
-        return false;
-    } else {
-        return true;
-    }
+    return !(number > top || number < bottom || !isNumber(number) || 
+        !isNumber(bottom) || !isNumber(top) ) 
 }
 
 function loadJS(path){
