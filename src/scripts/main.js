5da9e5ed08999cf69e97610df08b18d13e5858bb
kepet19 <kevinmeejach@gmail.com>
2020-12-13 21:21:57 +0100

    temperaty solution that does not work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,10 @@ import * as util from "./util.js"
 
 util.loadJS("./scripts/ui-elements/production-control.js");
 
-setTimeout(fecthMaintainsStatus, 1);
-setTimeout(fecthInventoryStatus, 1);
-
 // Production name and time
 const pTime = document.getElementById("pTime");
 const pName = document.getElementById("pName");
-let pId = 0;
+let batchId = 0;
 
 const btnTemp = document.getElementById("btnTemp");
 const btnState = document.getElementById("btnState");
@@ -23,42 +20,98 @@ const btnVib = document.getElementById("btnVib");
 const btnPPM = document.getElementById("btnPPM");
 const btnRP = document.getElementById("btnRP");
 
+const stateArray = []
+const tempArray = []
+const humArray = []
+const vibArray = []
+
+fecthMaintainsStatus()
+fecthInventoryStatus()
+
 fetch("https://api.bierproductie.nymann.dev/batches/latest/")
     .then(response => response.json())
     .then(data => {
-        pId = data.identifier;
+        batchId = data.identifier;
         btnPPM.innerText = data.speed;
         pName.innerText = data.recipe_id;
         btnATP.innerText = data.amount_to_produce;
+        setTimeout(fecthBtnData, 1);
     });
 
-fetch("https://api.bierproductie.nymann.dev/data_over_time/3?page_size=1&page=1")
-    .then(response => response.json())
-    .then(data => {
-        if (data.results[0] !== undefined) {
-            btnTemp.innerText = data.results[0].temperature;
-            btnState.innerText = MachineState[data.results[0].state];
-            btnProd.innerText = data.results[0].produced;
-            btnHum.innerText = data.results[0].humidity;
-            btnAP.innerText = data.results[0].produced - data.results[0].rejected;
-            btnVib.innerText = data.results[0].vibration;
-            btnRP.innerText = data.results[0].rejected;
-        } else {
-            console.log("no entries");
-        }
+const MachineState = {
+    0 : 'DEACTIVATED',
+        1 : 'CLEARING',
+        2 : 'STOPPED',
+        3 : 'STARTING',
+        4 : 'IDLE',
+        5 : 'SUSPENDED',
+        6 : 'EXECUTE',
+        7 : 'STOPPING',
+        8 : 'ABORTING',
+        9 : 'ABORTED',
+        10 : 'HOLDING',
+        11 : 'HELD',
+        15 : 'RESETTING',
+        16 : 'COMPLETING',
+        17 : 'COMPLETE',
+        18 : 'DEACTIVATING',
+        19 : 'ACTIVATING'
+}
 
-    });
+function fecthBtnData(){
+    fetch("https://api.bierproductie.nymann.dev/data_over_time/"+batchId+"?page_size=1&page=1")
+        .then(response => response.json())
+        .then(data => {
+            if (data.results[0] !== undefined) {
+                btnTemp.innerText = data.results[0].temperature;
+                btnState.innerText = MachineState[data.results[0].state];
+                btnProd.innerText = data.results[0].produced;
+                btnHum.innerText = data.results[0].humidity;
+                btnAP.innerText = data.results[0].produced - data.results[0].rejected;
+                btnVib.innerText = data.results[0].vibration;
+                btnRP.innerText = data.results[0].rejected;
+            } else {
+                console.log("no entries");
+            }
+        });
+    setTimeout(fecthBtnData, 10000);
+}
 
 
 
-new btn.button('humidity', "HUMIDITY");
-new btn.button('vibration', "VIBRATION");
-new btn.button('temp', "TEMPERATURE");
-new btn.button('state', "STATE");
+const graphHum = new btn.button('humidity', "HUMIDITY", humArray, [0,1,2,3,4,5]);
+const graphVib = new btn.button('vibration', "VIBRATION", vibArray, [0,1,2,3,4,5]);
+const graphTemp = new btn.button('temp', "TEMPERATURE" , tempArray, [0,1,2,3,4,5]);
+const graphState = new btn.button('state', "STATE", stateArray, [0,1,2,3,4,5]);
 // new btn.button('DefectProducts', "REJECTED PRODUCTS");
 // new btn.button('acceptableProducts', "ACCEPTABLE PRODUCTS");
 // new btn.button('produced', "PRODUCED");
 //
+function fecthGraphData(){
+    humArray.push(5);
+    graphUpdater();
+}   
+
+function graphUpdater(){
+    let marked = document.getElementsByClassName("marked")[0];
+    if(marked !== undefined){
+        if(marked.children[1].innerText == "HUMIDITY"){
+            console.log("HUMIDITY");
+            graphHum.update();
+        } else if(marked.children[1].innerText == "VIBRATION"){
+            graphVib.update();
+            console.log("VIBRATION");
+        }else if(marked.children[1].innerText == "TEMPERATURE"){
+            graphTemp.update();
+            console.log("TEMPERATURE");
+        }else if(marked.children[1].innerText == "STATE"){
+            graphState.update();
+            console.log("STATE");
+        }
+
+
+    }
+}
 
 function fecthInventoryStatus() {
     fetch("https://api.bierproductie.nymann.dev/inventory_statuses/?page_size=20&page=1")
@@ -82,22 +135,3 @@ function fecthMaintainsStatus(){
 
 
 
-const MachineState = {
-    0 : 'DEACTIVATED',
-        1 : 'CLEARING',
-        2 : 'STOPPED',
-        3 : 'STARTING',
-        4 : 'IDLE',
-        5 : 'SUSPENDED',
-        6 : 'EXECUTE',
-        7 : 'STOPPING',
-        8 : 'ABORTING',
-        9 : 'ABORTED',
-        10 : 'HOLDING',
-        11 : 'HELD',
-        15 : 'RESETTING',
-        16 : 'COMPLETING',
-        17 : 'COMPLETE',
-        18 : 'DEACTIVATING',
-        19 : 'ACTIVATING'
-}
