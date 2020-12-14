d5e52ec78573f9a3f66db863c2a8c03d863b15a4
kepet19 <kevinmeejach@gmail.com>
2020-12-14 02:12:11 +0100

    clean up properties.js

--- src/scripts/start-batch-main.js
+++ src/scripts/start-batch-main.js
@@ -6,12 +6,7 @@ util.loadJS("./scripts/ui-elements/properties.js");
 fetch('https://api.bierproductie.nymann.dev/recipes/')
 .then(response => response.json())
 .then(data => {
-    console.log(data);
     data.forEach(r => {
-        if(r.name === "alcoholFree"){
-            new recipe.recipe(r.name, r.max_speed, r.recommended_speed, 'alcohol free');
-        } else{
             new recipe.recipe(r.name, r.max_speed, r.recommended_speed);
-        }
     });
 });
