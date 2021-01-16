5a6aaca5e0df93beb71630c0b3c1de48797ede13
kepet19 <kevinmeejach@gmail.com>
2021-01-16 16:12:32 +0100

    Add a new way too count time

--- src/scripts/ui-elements/production-control.js
+++ src/scripts/ui-elements/production-control.js
@@ -1,6 +1,11 @@
+let x = 0;
+
 setInterval(function () {
-let time = document.getElementsByClassName("ptime")[0];
-    time.children[1].innerHTML = new Date()
-        .toString()
-        .slice(16, 25);
-}, 1);
+    let time = document.getElementsByClassName("ptime")[0];
+    time.children[1].innerHTML = getTime();
+}, 1000);
+
+function getTime() {
+    ++x;
+    return new Date(x * 1000).toISOString().substr(11, 8);
+}
